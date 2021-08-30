2e1fb7f844499c415b8d144b60a0549ee4006e2b
Raphael Zulliger <zulliger@software-natives.ch>
2021-08-30 20:44:52 +0200

    Use build context
    As suggested by https://github.com/conan-io/conan/issues/9494. `protoc` is now be found, but the grpc-plugin is not

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,5 @@
 from conans import ConanFile
+from conan.tools.cmake import CMakeDeps
 
 
 class TestGRPCConan(ConanFile):
@@ -8,4 +9,16 @@ class TestGRPCConan(ConanFile):
     url = ""
     generators = ["CMakeDeps", "CMakeToolchain"]
     settings = ["os", "compiler", "build_type", "arch"]
-    requires = ["grpc/1.38.0"]
+    requires = ["grpc/1.38.0", "protobuf/3.17.1"]
+    build_requires = ["grpc/1.38.0", "protobuf/3.17.1"]
+
+
+    def generate(self):
+        cmake = CMakeDeps(self)
+        # generate the config files for the build require
+        cmake.build_context_activated = ["grpc", "protobuf"]
+        # disambiguate the files, targets, etc
+        cmake.build_context_suffix = {"grpc": "_BUILD", "protobuf": "_BUILD"}
+        # Choose the build modules from "build" context
+        cmake.build_context_build_modules = ["grpc", "protobuf"]
+        cmake.generate()
