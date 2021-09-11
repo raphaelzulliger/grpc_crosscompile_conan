db2a0c95b7be74f08f2fd91868d5290d5320609a
Raphael Zulliger <zulliger@software-natives.ch>
2021-09-11 18:42:57 +0200

    Some experiments. Nothing really useful

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,4 @@
-from conans import ConanFile
+from conans import ConanFile, tools
 from conan.tools.cmake import CMakeDeps
 
 
@@ -7,18 +7,27 @@ class TestGRPCConan(ConanFile):
     version = "0.0.1"
     author = "Raphael Zulliger <zulliger@software-natives.ch>"
     url = ""
-    generators = ["CMakeDeps", "CMakeToolchain"]
+    generators = "cmake", "cmake_find_package"
     settings = ["os", "compiler", "build_type", "arch"]
     requires = ["grpc/1.38.0", "protobuf/3.17.1"]
-    build_requires = ["grpc/1.38.0", "protobuf/3.17.1"]
+    # build_requires = ["grpc/1.38.0", "protobuf/3.17.1"]
 
+    def build_requirements(self):
+        if hasattr(self, "settings_build") and tools.cross_building(self):
+            self.build_requires(str(self.requires['grpc']))
+            self.build_requires(str(self.requires['protobuf']))
 
-    def generate(self):
-        cmake = CMakeDeps(self)
-        # generate the config files for the build require
-        cmake.build_context_activated = ["grpc", "protobuf"]
-        # disambiguate the files, targets, etc
-        cmake.build_context_suffix = {"grpc": "_BUILD", "protobuf": "_BUILD"}
-        # Choose the build modules from "build" context
-        cmake.build_context_build_modules = ["grpc", "protobuf"]
-        cmake.generate()
+    # def build(self):
+    #     cmake = CMake(self)
+    #     cmake.configure()
+    #     cmake.build()
+
+    # def generate(self):
+    #     cmake = CMakeDeps(self)
+    #     # # generate the config files for the build require
+    #     # cmake.build_context_activated = ["grpc", "protobuf"]
+    #     # # disambiguate the files, targets, etc
+    #     # cmake.build_context_suffix = {"grpc": "_BUILD", "protobuf": "_BUILD"}
+    #     # # Choose the build modules from "build" context
+    #     # cmake.build_context_build_modules = ["grpc", "protobuf"]
+    #     cmake.generate()
